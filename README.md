9300a1e0dc937297db7588dc59e801b41e81afb0
kainhuck <1628321264@qq.com>
2023-03-21 17:43:20 +0800

    update readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# taobaosdk
+
+淘宝客 golang sdk
+
+## Usage
+
+see [exapmle](example)
--- example/main.go
+++ example/main.go
@@ -12,16 +12,14 @@ func main() {
 	appKey := os.Getenv("top_app_key")
 	appSecret := os.Getenv("top_app_secret")
 
+	// 新建client
 	client := taobaosdk.NewClient(appKey, appSecret)
 
+	// 淘宝客-公用-淘宝客商品详情查询(简版) | 所有支持的接口都位于 github.com/kainhuck/taobaosdk/api 下
 	itemInfo := api.NewItemInfoAPI()
-
-	itemInfo.Request.NumIids = "123,456"
-
-	err := client.Do(itemInfo)
-	if err != nil {
+	itemInfo.Request.NumIids = "123,456"        // 设置参数
+	if err := client.Do(itemInfo); err != nil { // 通过client发送请求
 		log.Fatal(err)
 	}
-
-	fmt.Println(itemInfo.Response)
+	fmt.Println(itemInfo.Response) // 获取返回信息
 }
